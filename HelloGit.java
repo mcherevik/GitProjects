5e975206297b881d8aaae77d087727bd23c76ccf
mcherevik <mcherevik1@gmail.com>
2022-10-19 01:05:30 +0300

    commited readme branch

--- HelloGit.java
+++ HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Whatsoever");
+        System.out.println("This can be read only in readme mode");
     }
 }
